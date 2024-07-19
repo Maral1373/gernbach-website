745b9d4a1315d660025908a1d76edfa8f1846894
Maral Erfanian <me.maralerfanian@gmail.com>
2024-07-19 10:51:32 +0200

    home images

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const mobileMenuButton = document.getElementById("mobile-menu-button");
 	const mobileMenu = document.getElementById("mobile-menu");
+	// hide menu on load
+	// mobileMenu.classList.toggle("hidden");
 
 	mobileMenuButton.addEventListener("click", function () {
 		mobileMenu.classList.toggle("hidden");
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,19 +11,20 @@ module.exports = {
 				tablet: "640px",
 				laptop: "1024px",
 				desktop: "1536px",
-				
+
 				// Breakpoint prefix	Minimum width	CSS
 				// sm	640px	@media (min-width: 640px) { ... }
 				// md	768px	@media (min-width: 768px) { ... }
 				// lg	1024px	@media (min-width: 1024px) { ... }
 				// xl	1280px	@media (min-width: 1280px) { ... }
 				// 2xl	1536px	@media (min-width: 1536px) { ... }
-				
 			},
 		},
 	},
 	variants: {
-		extend: {},
+		extend: {
+			backgroundColor: ["hover"],
+		},
 	},
 	plugins: [],
 };
