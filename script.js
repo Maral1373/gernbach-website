4194bf148ad747e6c3a4c5a8c35c859edd5bf2f5
Maral Erfanian <me.maralerfanian@gmail.com>
2024-07-21 21:11:03 +0200

    home context 3 boxes

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ document.addEventListener("DOMContentLoaded", function () {
 		mobileMenu.classList.toggle("hidden");
 	});
 });
+
+document.querySelectorAll(".collapsible-header").forEach((button) => {
+	button.addEventListener("click", () => {
+		const content = button.nextElementSibling;
+		const icon = button.querySelector("span:last-child");
+
+		if (content.style.display === "block") {
+			content.style.display = "none";
+			icon.innerHTML = "&#9654;";
+		} else {
+			content.style.display = "block";
+			icon.innerHTML = "&#9660;";
+		}
+	});
+});
